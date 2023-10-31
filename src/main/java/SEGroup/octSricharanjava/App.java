82aac89dfa95727277ea9daa1b07c59444658efd
Sricharan <21bd1a664ncsmc@gmail.com>
2023-10-31 16:03:23 +0530

    Demo for mavren

---
+++ src/main/java/SEGroup/octSricharanjava/App.java
@@ -0,0 +1,13 @@
+package SEGroup.octSricharanjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello all today is 31 oct 2023" );
+    }
+}
